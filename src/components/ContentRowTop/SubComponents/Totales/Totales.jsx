bd5f38021acceec75b42381f36ea88c6720aee28
franSlepoy <slepoyfrancisco@gmail.com>
2023-05-10 15:25:35 -0300

    iconos de copas de vino en Totales

--- src/components/ContentRowTop/SubComponents/Totales/Totales.jsx
+++ src/components/ContentRowTop/SubComponents/Totales/Totales.jsx
@@ -11,7 +11,9 @@ function Totales() {
 						  <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
 					</div>
 										<div className="col-auto">
-											<i className="fas fa-film fa-2x text-gray-300"></i>
+										    <i class="fa-solid fa-wine-glass-empty fa-shake fa-xl"></i>
+											<i class="fa-solid fa-wine-glass-empty fa-shake fa-xl"></i>
+											
 										</div>
 									</div>
 								</div>
