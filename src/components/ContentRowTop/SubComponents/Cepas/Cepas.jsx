fd7d2d7c06f0d6adc89610a27c37fe80e9d35a07
Fer-lando <fer.ram91@gmail.com>
2023-05-11 19:58:33 -0300

    Base para listado de cepas falta que conecte con la base

--- src/components/ContentRowTop/SubComponents/Cepas/Cepas.jsx
+++ src/components/ContentRowTop/SubComponents/Cepas/Cepas.jsx
@@ -1,89 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import CepasCard from "./subcomponents/CepasCard";
 
-function Cepas (){
-    return (
-        <div className="col-lg-6 mb-4">						
-        <div className="card shadow mb-4">
-            <div className="card-header py-3">
-                <h5 className="m-0 font-weight-bold text-gray-800">Cepas en la Base de Datos</h5>
-            </div>
-            <div className="card-body">
-                <div className="row">
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Malbec
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Cabernet Sauvignon
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Cabernet Franc
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Syrah
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Petit Verdot
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Pinot Noir
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Chardonnay
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Sauvignon Blanc
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Torrontés
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-4">
-                        <div className="card bg-dark text-white shadow">
-                            <div className="card-body">
-                                Semillón
-                            </div>
-                        </div>
-                    </div>
+class Cepas extends Component {
 
+    constructor(){
+        super()
+        this.state ={
+            cepaList: []
+        }
+    }
+
+    componentDidMount(){
+
+        fetch("http://localhost:3031/api/cepaList")
+        .then(respuesta =>{
+        return respuesta.json()
+        })
+        .then(vinos =>{
+        //console.log(cepas)
+        this.setState({cepaList: vinos.data})
+        })
+        .catch(error => console.log(error))
+        }
+
+    render() {
+        return (
+            <div className="col-lg-6 mb-4">
+                <div className="card shadow mb-4">
+                    <div className="card-header py-3">
+                        <h5 className="m-0 font-weight-bold text-gray-800">Cepas en la Base de Datos</h5>
+                    </div>
+                    <div className="card-body">
+                        {
+                        this.state.cepaList.map((cepa, i) => <CepasCard key={ cepa.cepas_idCepa + i } name={ cepa.cepas_idCepa } />)
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    )
+        )
+    }
 }
 export default Cepas
